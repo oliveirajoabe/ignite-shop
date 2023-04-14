1aa5bf0fd38f8295c1cd87aa66d2038e3ba79b2e
Joabe Oliveira <oliveira_joabe@hotmail.com>
2023-04-14 08:50:17 -0300

    configuração de stripe e exbir produtos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,69 @@
-import { styled } from "../styles"
+import Link from 'next/link'
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+import { stripe } from "../lib/stripe";
+import { GetStaticProps } from "next";
+import Stripe from "stripe";
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-  span: {
-    fontWeight: 'bold',
-  },
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+  }[]
+}
 
-export default function Home() {
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: { perView: 3, spacing: 48 }
+  })
+
   return (
-    <Button>
-      <span>teste</span>
-      enviar
-    </Button>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map((product) => {
+        return (
+          <Link key={product.id} href={`/product/${product.id}`}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} alt="" width={520} height={480} />
+
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
+        )
+      })}
+    </HomeContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(Number(price.unit_amount) / 100)
+    }
+  })
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
+  }
 }
